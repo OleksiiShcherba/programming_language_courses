173efe759691d607976539cde2a8d6abfbec5d58
viacheslav <lacostefracture@gmail.com>
2024-03-27 15:28:47 +0100

    my litle progres

---
+++ app/Http/Controllers/PhpController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class PhpController extends Controller
+{
+    public function goPhp(Request $request)
+    {
+        $path = ('/var/www/html/storage/phpCode/' . time() . '.php');
+        file_put_contents($path, '<?php' . "\n" . $request->code . "\n" . '?>');
+
+
+        return ($result);
+    }
+}
+
+
---
+++ storage/phpCode/1711549024.php
@@ -0,0 +1,3 @@
+<?php
+$a = 10;function add($e){return$e = $e+1;}$result=add($a);echo $result;
+?>
