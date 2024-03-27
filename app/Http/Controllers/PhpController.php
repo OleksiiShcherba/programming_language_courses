24f1d47604f0072eb66670bbcc9e68138fb99e95
viacheslav <lacostefracture@gmail.com>
2024-03-27 20:03:59 +0100

    code exec

--- app/Http/Controllers/PhpController.php
+++ app/Http/Controllers/PhpController.php
@@ -9,10 +9,10 @@ class PhpController extends Controller
 {
     public function goPhp(Request $request)
     {
+        $result = [];
         $path = ('/var/www/html/storage/phpCode/' . time() . '.php');
         file_put_contents($path, '<?php' . "\n" . $request->code . "\n" . '?>');
-
-
+        exec('php ' . $path, $result);
         return ($result);
     }
 }
